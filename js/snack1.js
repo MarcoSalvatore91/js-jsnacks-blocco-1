61bc418e1521e9c2b06b4aec445e40e51eb52c9c
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-11 15:05:52 +0100

    end exercise snack1

--- js/snack1.js
+++ js/snack1.js
@@ -1,1 +1,39 @@
 console.log('JS OK!')
+
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+// 1- Inserire due numeri (Prompt)
+// 2- Dettare le condizioni
+// 3- Stampare in pagina
+
+// ! Costante da stampare in pagina
+const display = document.getElementById('risultato');
+
+// 1- Inserire due numeri (Prompt)
+const firstNumber = parseInt(prompt('Scegli il primo numero', '2').trim());
+const secondNumber = parseInt(prompt('Scegli il secondo numero', '5').trim());
+
+console.log(firstNumber, secondNumber);
+
+// 2- Dettare le condizioni
+let result;
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    result = 'Inserire solo caratteri numerici!!!';
+    console.log(result);
+} else {
+    if (firstNumber > secondNumber) {
+    result = `Il valore ${firstNumber} è maggiore del valore ${secondNumber}`
+    console.log(result);
+} else if (firstNumber < secondNumber) {
+    result = `Il valore ${secondNumber} è maggiore del valore ${firstNumber}`
+    console.log(result);
+} else {
+    result = `I valori ${firstNumber} e ${secondNumber}, sono uguali`;
+    console.log(result);
+}
+}
+
+// ! Stampo in pagina il risultato
+display.innerHTML = result;
