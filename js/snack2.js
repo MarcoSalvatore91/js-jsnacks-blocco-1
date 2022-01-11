9faa97fa30b3732292a583fda9d54cb648bacc73
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-11 17:39:40 +0100

    end exercise snack2

--- js/snack2.js
+++ js/snack2.js
@@ -3,19 +3,30 @@ console.log('JS OK!')
 /*L’utente inserisce due parole in successione, con due prompt. 
 Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
+// 1-Inserire due parole in succession (Prompt)
+// 2- Stampare prima la parola piu' corta e dopo quella piu' lunga
+
+// ! Costante da stampare in pagina
 const result = document.getElementById('risultato');
 
+// 1-Inserire due parole in succession (Prompt)
 const firstWord = prompt('Inserisci la prima parola', 'Cane').trim();
 const secondWord = prompt('Inserisci la seconda parola', 'Gatto').trim();
 
-console.log(firstWord, secondWord);
+// 2- Stampare prima la parola piu' corta e dopo quella piu' lunga
+let lenghtWord = 'Le parole hanno la stessa lunghezza';
 
-let lenghtWord;
-if (firstWord.length > secondWord.length) {
-    lenghtWord = `${firstWord} ${secondWord}`
-} else if (firstWord.length < secondWord.length) {
-    lenghtWord = `${secondWord} ${firstWord}`
+if (firstWord.length === 0 || secondWord.length === 0) {
+    lenghtWord = 'Inserire almeno un carattere!!!';
+} else {
+    if (firstWord.length > secondWord.length) {
+        lenghtWord = `${secondWord} ${firstWord}`
+    } else if (firstWord.length < secondWord.length) {
+        lenghtWord = `${firstWord} ${secondWord}`
+    }
 }
+
 console.log(lenghtWord);
 
+// ! Stampo in pagina
 result.innerHTML = lenghtWord;
